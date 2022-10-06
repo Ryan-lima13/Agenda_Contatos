adfd5bb90bc67d19647daece7cae01981cd8ea21
ryanl <ryanlimadev@gmail.com>
2022-10-06 13:33:24 -0300

    autenticando usuario

--- app/src/main/java/activity/FormCadastro.java
+++ app/src/main/java/activity/FormCadastro.java
@@ -1,17 +1,99 @@
 package activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.rlds.agendacontatos.R;
+import com.rlds.agendacontatos.databinding.ActivityFormCadastroBinding;
+
+import helpe.ConfiguracaoFirebase;
+import model.Usuario;
 
 public class FormCadastro extends AppCompatActivity {
+    private ActivityFormCadastroBinding binding;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_cadastro);
+        binding = ActivityFormCadastroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        // fazer cadastro
+        binding.btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = binding.editNome.getText().toString();
+                String email = binding.editEmail.getText().toString();
+                String senha = binding.editSenha.getText().toString();
+                if( nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    binding.txtErro.setText("Preencha todos os campos!");
+                    binding.txtErro.setTextColor(getColor(R.color.red));
+
+                }else {
+                    binding.txtErro.setText("");
+                    usuario = new Usuario();
+                    usuario.setNome(nome);
+                    usuario.setEmail(email);
+                    usuario.setSenha(senha);
+                    // cadastrar
+                    cadastrarusario();
+
+                }
+            }
+        });
+    }
+    private  void cadastrarusario(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(FormCadastro.this, "usuario cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    String erro = "";
+
+                    try {
+                        throw  task.getException();
+
+                    }catch (FirebaseAuthWeakPasswordException e ){
+                        erro = "Digite senha com mais de 6 caracteres!";
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "Digite um email válido!";
+
+                    }catch (FirebaseAuthUserCollisionException e){
+                        erro = "conta já cadastrada!";
+
+                    }catch (Exception e){
+                        erro = "Erro ao fazer login:" + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    binding.txtErro.setText(erro);
+
+
+                }
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/activity/FormLogin.java
+++ app/src/main/java/activity/FormLogin.java
@@ -1,16 +1,27 @@
 package activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.rlds.agendacontatos.R;
 import com.rlds.agendacontatos.databinding.ActivityFormLoginBinding;
 
+import helpe.ConfiguracaoFirebase;
+import model.Usuario;
+
 public class FormLogin extends AppCompatActivity {
     private ActivityFormLoginBinding binding;
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +49,31 @@ public class FormLogin extends AppCompatActivity {
                 }else {
                     binding.txtErro.setText("");
                     // fazer login
+                    usuario = new Usuario();
+                    usuario.setEmail(email);
+                    usuario.setSenha(senha);
+                    logarUsuario();
                 }
             }
         });
+    }
+    private  void logarUsuario(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(FormLogin.this, "entrou",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(FormLogin.this, " não entrou",Toast.LENGTH_SHORT).show();
+
+
+                }
+
+            }
+        });
+
     }
 }
